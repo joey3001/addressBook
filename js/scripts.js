9600f3cf9c82c0f57d0b8d132d37731d51a3bdf2
Joey3001 <josephnero111@gmail.com>
2020-11-02 10:36:33 -0800

    fully functioning address book
    Co-Authored-By: Joseph Giunta <jgnohack@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,63 @@
 // Business Logic for addressBook 
-function addressBook() {
+function AddressBook() {
   this.contacts = [];
-  this.currentID = 0;
+  this.currentId = 0;
 }
 
-addressBook.prototype.addContact = function(contact) {
+AddressBook.prototype.addContact = function(contact) {
   contact.id = this.assignId();
   this.contacts.push(contact);
 }
 
-addressBook.prototype.assignId = function() {
+AddressBook.prototype.assignId = function() {
   this.currentId += 1; 
   return this.currentId; 
 }
 
+AddressBook.prototype.findContact = function(id) {
+  for (let i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (let i = 0; i < this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
 // Business Logic for Contacts 
-function contact(firstName, lastName, phoneNumber) {
+function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName; 
   this.lastName = lastName; 
   this.phoneNumber = phoneNumber; 
 }
 
-contact.prototype.fullName = function() { 
+Contact.prototype.fullName = function() { 
   return this.firstName + " " + this.lastName; 
 }
+
+Contact.prototype.update = function() {
+
+}
+/*
+let addressBook = new AddressBook();
+let contact = new Contact("Ada", "Lovelace", "503-555-0100");
+let contact2 = new Contact("Grace", "Hopper", "503-555-0199");
+addressBook.addContact(contact);
+addressBook.addContact(contact2);
+*/
+
+// const fooBar = new Contact("Joseph", "Giunta", 503);
+// console.log(fooBar.lastName)
